34f2a49ca9555f537b9d46c9358f175349fd91d2
alexhoedlmoser <alex.hoedlmoser@gmail.com>
2019-05-26 20:16:24 +0200

    added Collectibles, Player, Lives-, Timer-, Score-GUI elements as Prefabs, also added Collectible scripts for blossom and raindrop, added basic Player movement

---
+++ Assets/MyGame/Scripts/CollectibleBlossom.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class CollectibleBlossom : MonoBehaviour
+{
+    private int score = 0;
+    public TextMeshProUGUI scoreGui;
+
+    void Start()
+    {
+        scoreGui.text = score.ToString();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Triggerevent" + collision.gameObject.name);
+
+        if (collision.gameObject.name == "PlayerBee")
+        {
+            score += 1;
+            Debug.Log("score: " + score);
+            scoreGui.text = score.ToString();
+            Destroy(gameObject, 0f);
+        }
+        else if (collision.gameObject.name == "BottomHitbox")
+        {
+            score -= 1;
+            Debug.Log("score: " + score);
+            scoreGui.text = score.ToString();
+            Destroy(gameObject, 1f);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("CollisionEvent");
+    }
+
+}
---
+++ Assets/MyGame/Scripts/CollectibleRaindrop.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class CollectibleRaindrop : MonoBehaviour
+{
+    private int lives = 3;
+    public TextMeshProUGUI livesGui;
+
+    void Start()
+    {
+        lives = 3;
+        livesGui.text = lives.ToString();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Triggerevent" + collision.gameObject.name);
+
+        if (collision.gameObject.name == "PlayerBee")
+        {
+            lives -= 1;
+            Debug.Log("lives: " + lives);
+            livesGui.text = lives.ToString();
+            Destroy(gameObject, 0f);
+        }
+        else if (collision.gameObject.name == "BottomHitbox")
+        {
+            Destroy(gameObject, 1f);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("CollisionEvent");
+    }
+
+}
