e1545f3d204d9419645c8d60c521bdac6905d860
alexhoedlmoser <alex.hoedlmoser@gmail.com>
2019-06-15 19:28:10 +0200

    added new mouseCursor with particle effects, added new splash screen

---
+++ Assets/MyGame/Scripts/mouseCursor.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseCursor : MonoBehaviour
+{
+    [SerializeField] GameObject clickEffect;
+    [SerializeField] GameObject trailEffect;
+    private float timeBtwSpawn = 0.01f;
+
+	void Start () {
+        Cursor.visible = false;
+	}
+	
+	void Update () {
+        Vector2 cursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = cursorPos;
+
+        if(Input.GetMouseButtonDown(0))
+        {
+            Instantiate(clickEffect, transform.position, Quaternion.identity);
+        }
+
+        if(timeBtwSpawn <= 0)
+        {
+            Instantiate(trailEffect, cursorPos, Quaternion.identity);
+            timeBtwSpawn = 0.01f;
+        }
+        else
+        {
+            timeBtwSpawn -= Time.deltaTime;
+        }
+	}
+}
